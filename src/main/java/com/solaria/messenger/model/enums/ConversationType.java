eeeaa225ddf476e45c2dea2ea0d4ac83b39dd1da
Arthur Machado <arthur.machado2501@gmail.com>
2026-08-21 20:36:01 -0300

    feat: (#5)

---
+++ src/main/java/com/solaria/messenger/model/enums/ConversationType.java
@@ -0,0 +1,6 @@
+package com.solaria.messenger.model.enums;
+
+public enum ConversationType {
+    USER_CONVERSATION,
+    CHAT_BOT
+}
---
+++ src/main/java/com/solaria/messenger/model/enums/MessageType.java
@@ -0,0 +1,7 @@
+package com.solaria.messenger.model.enums;
+
+public enum MessageType {
+    USER_TO_USER,
+    USER_TO_CHATBOT,
+    CHATBOT_TO_USER
+}
